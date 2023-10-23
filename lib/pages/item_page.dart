35b6fed5295424662725ce2e8db068ff29961f86
taufiqyfirdaus <taufiqyfirdaus@gmail.com>
2023-10-23 15:15:56 +0700

    Praktikum 5 + Tugas 2

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    // Membaca nilai yang dikirimkan dari halaman sebelumnya
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Item Details'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Item Name: ${itemArgs.name}'),
+            Text('Item Price: ${itemArgs.price.toString()}'),
+          ],
+        ),
+      ),
+    );
   }
 }
